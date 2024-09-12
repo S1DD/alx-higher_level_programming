58cc348250ac889aa79c1fb1b6f9d366238011e8
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-11 17:37:29 -0700

    Completed

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 5 == 0:
+            print("Buzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        else:
+            print("{n} ,", end="")
+        
