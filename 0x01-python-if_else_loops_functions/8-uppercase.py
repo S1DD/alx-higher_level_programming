fc523158d068341352383d0f834629e22c57c00d
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-11 17:33:57 -0700

    Completed

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
+
+def uppercase(s):
+    new = ""
+    for character in s:
+        new += "%c" % to_uper(character)
+    print("{:s}".format(new))
+        
