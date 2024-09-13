f8919ddd579a16c801c1a26153ccfa8a05fda286
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-13 15:56:59 -0700

    Complete

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1
-    if n == 0:
+    if count == 0:
         print("0 arguments.")
-    elif n == 1:
+    elif count == 1:
         print("1 argument.")
     else:
         print("{} arguments:".format(count))
