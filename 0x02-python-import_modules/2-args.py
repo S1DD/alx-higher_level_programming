fe4a81e1f6bc6f7f8ea1f515d96a4fb489d700c4
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-13 15:54:49 -0700

    Changed variable name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
 
-    n = len(sys.argv) - 1
+    count = len(sys.argv) - 1
     if n == 0:
         print("0 arguments.")
     elif n == 1:
         print("1 argument.")
     else:
-        print("{} arguments:".format(n))
-    for i in range(n):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
