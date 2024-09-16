fcf83a488c6abebafe942c0631f598907c074cbb
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 07:03:31 -0700

    Fixed logic for printing out the list after reversing it

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,6 @@ def print_reversed_list_integer(my_list=[]):
     """Prints the elements of a list in reverse"""
     if isinstance(my_list, list):
         my_list.reverse()
-
-    for i in mylist):
+        for i in mylist):
         print("{:d}".format(i))
     
