f32b18eac6b27836fdaabe725b0ccda306e97eb7
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 06:23:37 -0700

    Task Complete

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(matrix):
+        for j in range(matrix[i]):
+            print("{:d}".format(matrix[i][j], end="")
+                  if j != (matrix[i] - 1:
+                  print(" ", end="")
