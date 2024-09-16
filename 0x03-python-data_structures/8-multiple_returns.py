1e73a0dde5108951a27c862692d2bf3dcf31eb6d
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 08:05:32 -0700

    Forgot to add a return statement if the string is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 def multiple_returns(sentence):
     """Returns a tuple containing the length of a sentence and the first character"""
     if sentence == "":
-        (0, None)
+        return (0, None)
 
     return (len(sentence), sentence[0])
