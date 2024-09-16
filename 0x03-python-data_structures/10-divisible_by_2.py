ede8325c84ff8e7f3678325e0b1cf45001a75881
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 07:43:19 -0700

    Task Completed: removed unnecessary variable

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,9 @@
 
 def divisible_by_2(my_list=[]):
     multiples_of_2 = []
-    div_by_2 = 2
 
     for value in range(len(my_list)):
-        if value % div_by_2 == 0:
+        if value % 2 == 0:
             multiples_of_2.append(True)
         else:
             multiples_of_2.append(False)
