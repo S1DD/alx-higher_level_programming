d01c2c49d3961f0409a8b1f3496ca29a4ff10e40
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 09:00:25 -0700

    Fixed errors and indentation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,6 @@ def print_reversed_list_integer(my_list=[]):
     """Prints the elements of a list in reverse"""
     if isinstance(my_list, list):
         my_list.reverse()
-        for i in mylist):
-        print("{:d}".format(i))
+        for i in mylist:
+            print("{:d}".format(i))
     
