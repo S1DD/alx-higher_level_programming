0b2cb1b221e94f301c7c527904f169174638631a
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-09-16 07:55:16 -0700

    Corrected logic when accessing the elements in the list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,9 @@ def divisible_by_2(my_list=[]):
     multiples_of_2 = []
 
     for value in range(len(my_list)):
-        if value % 2 == 0:
+        if my_list[value] % 2 == 0:
             multiples_of_2.append(True)
         else:
             multiples_of_2.append(False)
 
-    return multiples_of_2
+    return (multiples_of_2)
