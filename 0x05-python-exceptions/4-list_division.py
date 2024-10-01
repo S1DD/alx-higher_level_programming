d6696c73077030c1b81e45f7973f603d56369773
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-01 16:59:11 -0700

    Completed tasks 3-6

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def list_division(my_list1, my_list2,list_length):
+
+    new_list = []
+
+    for i in range(0, list_length):
+        try:
+            result = my_list1[i] / my_list2[i]
+
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(result)
+    return (new_list)
+    
