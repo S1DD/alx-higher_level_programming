0b26b4e0a8caa5d221539a8039f7581859600f82
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-01 17:30:05 -0700

    Corrected typos in function parameter names

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-def list_division(my_list1, my_list2,list_length):
+def list_division(my_list_1, my_list_2,list_length):
 
     new_list = []
 
     for i in range(0, list_length):
         try:
-            result = my_list1[i] / my_list2[i]
-
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             div = 0
@@ -20,5 +19,5 @@ def list_division(my_list1, my_list2,list_length):
         finally:
             pass
         new_list.append(result)
-    return (new_list)
+    return new_list
     
