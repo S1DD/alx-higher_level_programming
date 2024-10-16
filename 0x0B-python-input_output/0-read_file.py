d8eb7253632902692c5b5207a69a9a9530a90546
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-15 18:42:45 -0700

    Fixed indentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
 
-def read_file(filename="", encoding="utf-8"):
-    """ Reads from a text file and prints
-        it to stdout
 
-        Args:
-             filename: The file to read
-    """
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
