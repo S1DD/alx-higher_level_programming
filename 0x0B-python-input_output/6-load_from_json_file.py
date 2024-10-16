d748a83fbcee555cf593bf32496b7ab07a1bb12a
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-15 19:01:37 -0700

    Fixed logic error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
+"""Defines a JSON file-reading function."""
 import json
-"""Defines a function that creates an Object from a JSON file"""
 
 
 def load_from_json_file(filename):
-    """Creates an object from a JSON file"""
+    """Create a Python object from a JSON file."""
     with open(filename) as f:
-        json.load(f)
+        return json.load(f)
