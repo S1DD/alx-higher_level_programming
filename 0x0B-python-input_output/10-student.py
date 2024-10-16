e588975e3a221519b38118017dafe6defdd96361
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-15 19:07:35 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
-"""Defines student class"""
+"""Defines a class Student."""
 
 
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes a new student
+        """Initialize a new Student.
+
         Args:
-             first_name: (str) Name of the student
-             last_name: (str) Last nmae of the student
-             age: (int) Age of the student
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -25,6 +27,6 @@ class Student:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
-                all(type(el) == str for el in attrs)):
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
