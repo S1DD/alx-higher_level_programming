23ace006eaf5f1bc2b73d2603a0596c4e3161ca6
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-10-16 16:54:04 -0700

    Fixed logic

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,4 +15,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
+    def area(self):
+        """ Returns area of Square object"""
+        return super().area()
